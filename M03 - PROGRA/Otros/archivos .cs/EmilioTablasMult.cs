580d4c4e1b038e340db8c8b4d0e3d1d57b67a515
Emilio Paris <56090831+eparmon126@users.noreply.github.com>
2023-03-17 18:25:15 +0100

    Add files via upload
    Ejercicio de un curso de Udemy. Pide un número y muestra su tabla de multiplicar.

---
+++ M03 - PROGRA/Otros/archivos .cs/EmilioTablasMult.cs
@@ -0,0 +1,23 @@
+﻿namespace TablasMult
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio Udemy Sección 3 parte 12. Tablas de multiplicar
+            //Pide un número al usuario y muestra la tabla de multiplicar.
+            int i = 0;
+            int mult = 0;
+            Console.WriteLine("Introduce un número para ver su tabla de multiplicar");
+            string lee = Console.ReadLine();
+            int n = int.Parse(lee);
+
+            Console.WriteLine("La tabla del {0} es:", n);
+
+            while (mult < 11; i++)
+            {
+
+            }
+        }
+    }
+}
