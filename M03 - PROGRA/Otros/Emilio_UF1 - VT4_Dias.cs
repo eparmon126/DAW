587f2d5314f01e8c514e9f902a76b176552037b9
Emilio Paris <56090831+eparmon126@users.noreply.github.com>
2023-03-17 18:27:58 +0100

    Add files via upload
    (UF1 - VT4) Ejercicio Voluntario. Pide un número de días y muestra cuantos años, meses, semanas  y días son.

---
+++ M03 - PROGRA/Otros/Emilio_UF1 - VT4_Dias.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejer_VT_Dias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe el número de días");
+            int ini_dias = int.Parse(Console.ReadLine());
+
+            int años = ini_dias / 365;
+            int meses = (ini_dias % 365) / 30;
+            int semanas = (((ini_dias % 365) % 30)/7);
+            int dias = (semanas % 7);
+
+            Console.WriteLine($"{ini_dias} son {años}, {meses}, {semanas}, {dias}");
+
+        }
+    }
+}
