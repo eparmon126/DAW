f981a3d7af5d6d35bba8da2edc1738dcc55ae4f6
Emilio Paris <56090831+eparmon126@users.noreply.github.com>
2023-03-17 18:29:52 +0100

    Add files via upload
    (UF1 - VT4) Pide 10 números y muestra cuantos de ellos son pares.

---
+++ M03 - PROGRA/Programacion secuencial/archivos .cs/Emilio_UF1_VT4_Pares.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejer_VT_ArrayPares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] vector = new int[10];
+            int cuenta = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Escribe el valor número {i + 1}");
+                vector[i] = Convert.ToInt32(Console.ReadLine());
+                if (vector[i] % 2 == 0)
+                {
+                    cuenta++;
+                    Console.Beep(1500, 200);
+                    Console.Beep(3000, 500);
+                }
+                else
+                {
+                    Console.Beep(400, 600);
+                }
+            }
+
+            Console.WriteLine($"Has introducido {cuenta} números pares");
+                      
+        
+        }
+    }
+}
